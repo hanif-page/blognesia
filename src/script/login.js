507579179b124e2d2b5a1b5432748ac4f38fa802
hanif-sudoapt <amrhanif15@gmail.com>
2021-10-07 15:45:45 +0700

    finish make the login page

--- src/script/login.js
+++ src/script/login.js
@@ -4,3 +4,9 @@ const daerahPage = (kota) => {
     let URL = rawURL + kota 
     window.open(URL, "_self")
 }
+
+// go to the home page
+const homePage = () => {
+    let URL = "../../"
+    window.open(URL, "_self")
+}
